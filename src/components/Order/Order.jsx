b2ef9e74f6f26e2487c878fa80d00ada33916cef
Allan Miller <millerallan17@gmail.com>
2024-03-20 12:05:56 -0300

    add: filtro de ordem alfabetica, com completas e incompletas atualizado

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Order.css';
+import AppContext from '../../context/AppContext';
+
 function Order() {
+  const { taskList, setTaskList,filteredTaskList, setFilteredTaskList } = useContext(AppContext);
+
+  const handleTaskListAsc = () => {
+    const sortedList = [...taskList].sort((a, b) => a.text.localeCompare(b.text)); // isso ordena por ordem alfabetica
+    setTaskList(sortedList);
+    const sortedFilteredList = [...filteredTaskList].sort((a, b) => a.text.localeCompare(b.text)); // caso tenho o filtro de completas ou imcompletas
+    setFilteredTaskList(sortedFilteredList);
+  };
+
+  const handleTaskListDesc = () => {
+    const sortedList = [...taskList].sort((a, b) => b.text.localeCompare(a.text)); // isso ordena por ordem alfabetica ao contrário
+    setTaskList(sortedList);
+    const sortedFilteredList = [...filteredTaskList].sort((a, b) => b.text.localeCompare(a.text)); // caso tenho o filtro de completas ou imcompletas
+    setFilteredTaskList(sortedFilteredList);
+  };
+
   return (
     <div className="order">
       <p className="order__title">Ordem alfabética</p>
       <div className="order__buttons">
-        <button type="button" className="order__button">Asc </button>
-        <button type="button" className="order__button last">Desc</button>
-
+        <button type="button" className="order__button" onClick={handleTaskListAsc}>
+          Asc
+        </button>
+        <button type="button" className="order__button last" onClick={handleTaskListDesc}>
+          Desc
+        </button>
       </div>
-    </div>);
+    </div>
+  );
 }
 
 export default Order;
